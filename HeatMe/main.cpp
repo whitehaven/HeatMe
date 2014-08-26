032310fae1a34cba55f44dc563c96d306fda7b2c
whitehaven <accrist@me.com>
2014-08-26 12:40:04 -0600

    Initial Commit

---
+++ HeatMe/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HeatMe
+//
+//  Created by Alex Crist on 8/26/14.
+//  Copyright (c) 2014 White Haven Informatics. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
